07cc435c3a78dd3097841dd4fad57734b95469b4
MuradRashidov <muradrashidov995@gmail.com>
2024-12-05 01:02:20 +0400

    test

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,6 +12,7 @@ const start = async () => {
     if(!process.env.NATS_URL) throw new Error("NATS_URL must be defined");
     if(!process.env.NATS_CLUSTER_ID) throw new Error("NATS_CLUSTER_ID must be defined");
     try {
+        //jkjlllk
         await natsWrapper.connect(process.env.NATS_CLUSTER_ID,process.env.NATS_CLIENT_ID,process.env.NATS_URL);
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed!');
