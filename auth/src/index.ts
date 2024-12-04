d6b84d1fd952f5dbcef72b8027731bfed7f99e74
MuradRashidov <muradrashidov995@gmail.com>
2024-12-04 21:32:02 +0400

    test reach docker from github action

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-    console.log('testttt');
+    console.log('testtttt');
     //asddfdasf
     if(!process.env.JWT_KEY) throw new Error("JWT must be defined");
     if(!process.env.MONGO_URI) throw new Error("JWT must be defined");
